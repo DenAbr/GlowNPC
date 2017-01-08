04062bb37a3a4e544ff71ccffc7e15e187de41e0
DenAbr <ia123@Tablet228>
2017-01-08 17:14:46 +0700

    First commit

---
+++ src/ru/den_abr/glownpc/NPCEntityDescriptor.java
@@ -0,0 +1,13 @@
+package ru.den_abr.glownpc;
+
+import org.bukkit.plugin.Plugin;
+
+import net.glowstone.entity.CustomEntityDescriptor;
+
+public class NPCEntityDescriptor extends CustomEntityDescriptor<GlowNPC> {
+
+    public NPCEntityDescriptor(Plugin plugin) {
+        super(GlowNPC.class, plugin, "NPC", new NPCStore());
+    }
+
+}
